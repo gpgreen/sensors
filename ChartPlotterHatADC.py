d3459f30f88b14c2c40223c3830d41ede7306991
Greg Green <gpgreen@gmail.com>
2021-02-01 16:58:47 -0800

    Created LM35 and Thermistor class to convert raw ADC
    Reduced SPI speed to 32000hz
    Selected both A0 and A1 for reading

--- ChartPlotterHatADC.py
+++ ChartPlotterHatADC.py
@@ -17,51 +17,32 @@ class ChartPlotterHatADC:
         if bus != 0:
             raise ValueError("SPI bus 1 not implemented yet")
         self._spi_dev = device
+        if device < 0 or device > 1:
+            raise ValueError("Illegal channel:{}".format(device))
         self._spi_handle = None
-        self._temp = 0
         self._button = button
-        print("ChartPlotterHat initialized on SPI bus {} device {}".format(
+        print("ChartPlotterHat initialized on SPI bus {} channel {}".format(
             self._spi_bus, self._spi_dev))
 
     def open(self):
-        self._spi_handle = self._gpiod.spi_open(self._spi_dev, 100000, 0)
+        self._spi_handle = self._gpiod.spi_open(self._spi_dev, 32000, 0)
         time.sleep(0.00002)
         # read firmware version
         fwver = self._spi_write([0x4, 0x0, 0x0])
         print("firmware:{}.{}".format(fwver[0], fwver[1]))
         # specify which channel to get
-        self._spi_write([0x1, 0x2, 0x00])
+        self._spi_write([0x1, 0x3, 0x00])
         print("channels:", self._spi_write([0x2, 0x0, 0x0])[0])
 
     def close(self):
         self._gpiod.spi_close(self._spi_handle)
 
-    def read_lm35(self):
-        res = self._spi_write(self._send(0))
+    def read_adc_channel(self, channel):
+        if channel < 0 or channel > 7:
+            raise ValueError("Illegal ADC Channel: {}".format(channel))
+        res = self._spi_write([channel + 0x10, 0, 0])
         raw = res[0] + (res[1] << 8)
-        self._temp = raw * 3.3 / 1023.0 * 100
-        print("raw temp:", raw)
-
-    def read_thermistor(self):
-        res = self._spi_write(self._send(1))
-        raw = res[0] + (res[1] << 8)
-        print("raw thermistor:", raw)
-
-    def _send(self, channel):
-        return [channel + 0x10, 0, 0]
-
-    def temperature(self):
-        return self._temp
-
-    def create_nmea0183_sentence(self, talker_id):
-        nmea_sentence = '{}MTA,{:.1f},C'.format(talker_id, self._temp)
-        # compute checksum
-        checksum_value = 0
-        for nmea_ch in nmea_sentence:
-            checksum_value ^= ord(nmea_ch)
-        nmea_sentence = '${}*{:02x}\r\n'.format(nmea_sentence, checksum_value)
-        #print(nmea_sentence[:-2])
-        return nmea_sentence
+        return raw
 
     def _spi_write(self, data):
         # pulse button pin, to wake up device
@@ -70,8 +51,12 @@ class ChartPlotterHatADC:
         time.sleep(0.000005)
         self._gpiod.write(self._button, 1)
         self._gpiod.set_mode(self._button, pigpio.INPUT)
+        time.sleep(0.0001)
+        self._gpiod.write(8, 0)
         time.sleep(0.00005)
         # now write to the dev
         self._gpiod.spi_xfer(self._spi_handle, data[:1])
         time.sleep(0.00004)
-        return self._gpiod.spi_xfer(self._spi_handle, data[1:])[1]
+        data = self._gpiod.spi_xfer(self._spi_handle, data[1:])[1]
+        self._gpiod.write(8, 1)
+        return data
--- sensor_monitor.py
+++ sensor_monitor.py
@@ -14,10 +14,54 @@ import socket
 import time
 import json
 import os
+import math
 
 import pigpio
 import ChartPlotterHatADC
 import bmp085
+
+class LM35:
+    """Takes ADC readings and converts to temperature for an LM35 sensor"""
+    def __init__(self):
+        self._temp = 0
+
+    def adc_reading(self, adc_value):
+        self._temp = adc_value * 3.3 / 1023.0 * 100
+        print("lm35 temp:", self._temp)
+
+    def create_nmea0183_sentence(self, talker_id):
+        nmea_sentence = '{}MTA,{:.1f},C'.format(talker_id, self._temp)
+        # compute checksum
+        checksum_value = 0
+        for nmea_ch in nmea_sentence:
+            checksum_value ^= ord(nmea_ch)
+        nmea_sentence = '${}*{:02x}\r\n'.format(nmea_sentence, checksum_value)
+        #print(nmea_sentence[:-2])
+        return nmea_sentence
+
+class Thermistor:
+    """Takes ADC readings and converts to temperature for an Thermistor sensor"""
+    def __init__(self):
+        self._steinhart = 0
+
+    def adc_reading(self, adc_value):
+        resistance = 10000 / (1023.0/adc_value-1.0)
+        steinhart = resistance/10000.0
+        steinhart = math.log(steinhart)
+        steinhart += 1.0 / (25 + 273.15)
+        steinhart = 1.0 / steinhart
+        self._steinhart = steinhart - 273.15
+        print("steinhart:", self._steinhart)
+
+    def create_nmea0183_sentence(self, talker_id):
+        nmea_sentence = '{}MTA,{:.1f},C'.format(talker_id, self._steinhart)
+        # compute checksum
+        checksum_value = 0
+        for nmea_ch in nmea_sentence:
+            checksum_value ^= ord(nmea_ch)
+        nmea_sentence = '${}*{:02x}\r\n'.format(nmea_sentence, checksum_value)
+        #print(nmea_sentence[:-2])
+        return nmea_sentence
 
 def read_config_file(fname):
     config = None
@@ -61,12 +105,14 @@ def main():
                                       int(config['bmp085_xclr_pin']),
                                       int(config['bmp085_eoc_pin']))
 
+        thermistor = Thermistor()
+        lm35 = LM35()
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             while True:
-                hatdev.read_lm35()
-                hatdev.read_thermistor()
+                lm35.adc_reading(hatdev.read_adc_channel(0))
+                thermistor.adc_reading(hatdev.read_adc_channel(1))
                 barodev.read_sensor()
-                sock.sendto(hatdev.create_nmea0183_sentence(talker_id).encode(),
+                sock.sendto(lm35.create_nmea0183_sentence(talker_id).encode(),
                             (udp_host, udp_port))
                 sock.sendto(barodev.create_nmea0183_sentence(talker_id).encode(),
                             (udp_host, udp_port))
